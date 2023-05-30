f7be91e148e5cfe445a6ca570b2d93c86ff0187b
wangbing0407 <949247345@qq.com>
2023-05-30 13:48:19 +0800

    增加npm run all

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -2,20 +2,20 @@
   <div id="app">
     <div class="menu-left">
       <el-menu :router="true" default-active="2" class="el-menu-vertical-demo"  @open="handleOpen"
-      @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-      <el-menu-item index="/app-vue-hash">
-        <i class="el-icon-menu"></i>
-        <span slot="title">vue2子应用</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
-    </el-menu>
+        @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu-item index="/app-vue-hash">
+          <i class="el-icon-menu"></i>
+          <span slot="title">vue2子应用</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
     </div>
     <div id="appContainer"></div>
     <router-view/>
