3309f31a4ccdfb4f7840bc654dfafd9d18d54f2d
wangbing0407 <949247345@qq.com>
2023-05-31 08:58:18 +0800

    导航调整

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -7,9 +7,9 @@
           <i class="el-icon-menu"></i>
           <span slot="title">vue2子应用</span>
         </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
+        <el-menu-item index="3">
+          <i class="el-icon-menu"></i>
+          <span slot="title">vue3子应用</span>
         </el-menu-item>
         <el-menu-item index="4">
           <i class="el-icon-setting"></i>
